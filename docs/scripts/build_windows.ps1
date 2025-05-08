b0113913f02a43da69446cadd5a0d28575c6d575
Phatcat <holsthein@live.dk>
2025-05-08 19:54:27 +0200

    Cached native buildrunners
    Hitting all 3 target platforms with debug builds targeting both x64 and arm64, just like the upcoming manual build guide.
    
    - The windows build tests the vcpkg configuration and reveals any issues in that install path.
    - macOS uses a homebrew approach and only needs to get the binaries from mysql qhich takes about a second.
    Add a simple check in the top level cmakefile and add homebrew libraries if we're on apple so the buildbot and people can use natively installed libraries.
    - Linux uses a mix between apt and homebrew
    - The linux docker already tests a native dpendency path, and most packages can be got from apt-get with the exception of a proper mysql, but that takes but a second to download and install.
    
    all caches are about 700mb-1gb in size, and with the current 4 native builders that's about 3gb in total so there's room for expansion

---
+++ docs/scripts/build_windows.ps1
@@ -0,0 +1,61 @@
+# Copyright (c) 2025 Ember
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#############################################################
+# Install dependencies through vcpkg
+# Just grab and bootstrap the vcpkg and the toolchain file will handle the rest
+#############################################################
+Write-Host "Cloning vcpkg and boot-strapping"
+git clone https://github.com/microsoft/vcpkg.git
+.\vcpkg\bootstrap-vcpkg.bat
+.\vcpkg\vcpkg integrate install
+
+###############################
+# Configure and Build Ember
+###############################
+Write-Host "=== Configuring project with CMake ==="
+
+# Check if we are running on an ARM architecture.
+if ($env:PROCESSOR_ARCHITECTURE -eq "ARM64") {
+    Write-Host "ARM64 architecture detected on this build runner."
+    $targetTriplet= "arm64-windows-static"
+} else {
+    Write-Host "Non-ARM architecture detected."
+    $targetTriplet = "x64-windows-static"
+}
+
+$buildDir            = "build"
+$installDir          = ".\build\bin"
+$generator           = "Visual Studio 17 2022"
+$toolchainFile       = "vcpkg\scripts\buildsystems\vcpkg.cmake"
+$buildOptionalTools  = "-1"
+$disableEmberThreads = "0"
+$runtimeOption       = "MultiThreaded$<$<CONFIG:Debug>:Debug>"
+$buildType           = "Debug"
+
+cmake -S . -B $buildDir -G "$generator" `
+      -DCMAKE_TOOLCHAIN_FILE="$toolchainFile" `
+      -DVCPKG_TARGET_TRIPLET="$targetTriplet" `
+      -DCMAKE_MSVC_RUNTIME_LIBRARY="$runtimeOption" `
+      -DBUILD_OPT_TOOLS="$buildOptionalTools" `
+      -DDISABLE_EMBER_THREADS="$disableEmberThreads" `
+      -DCMAKE_INSTALL_PREFIX="$installDir"
+
+Write-Host "Building and installing the project..."
+cmake --build $buildDir --target install --config "$buildType"
+
+###############################################
+# Run the unit_tests for regression control
+###############################################
+Write-Host "=== Switching to installed directory and running tests ==="
+Set-Location $installDir
+if (Test-Path ".\unit_tests.exe") {
+    .\unit_tests.exe
+} else {
+    Write-Host "Warning: Installed test executable not found."
+}
+
+Write-Host "=== Build, install, and test complete ==="
